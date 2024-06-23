63dcae7f8cd18ed86bd013b633cd8dba9cfd6944
crmscope <alexei.kukharenko@gmail.com>
2024-06-23 22:14:53 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Рецепьы</title>
+		<style>
+			td {                
+                border: solid 1px silver; 
+            }		
+            .td-description {
+                width: 960px; 
+                height:60px; 
+                border: solid 1px silver; 
+                text-align: left;
+				padding-left: 20px;
+            }
+			.td-video {
+				width: 500px; 
+			}
+		</style>	
+	</head>
+	<body>
+	
+		<table border=0 width="100%">
+			
+			
+			
+			<tr>
+				<td colspan="3" align="center"><h3>НОВИНКА!!!Супер НЕЖНЫЕ!!! Горячие бутерброды с КРАБОВЫМИ палочками. ОЧЕНЬ ВКУСНО!!!</h3></td>
+			</tr>
+			<tr>        
+				<td class="td-video">
+					<iframe width="500" height="315" src="https://www.youtube.com/embed/BmTnzj1SuYs?si=_xHgs5c-mfs88LsJ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				</td>
+				<td class="td-description">			
+					<p>Очень нежные бутерброды в духовке на завтрак за 10 минут. Попробовала в ресторане, теперь часто готовлю дома</p>
+					<p>Друзья, если вам понравился видео - вы можете помочь каналу: </p>
+					<p>ПОДЕЛИТЕСЬ ЭТИМ ВИДЕО С ДРУЗЬЯМИ в соц. сетях + рецепт сохранится у вас на странице!</p>
+					<p>ОЦЕНИВАЙТЕ ВИДЕО!  - это приятно для меня и важно для развития канала!</p>
+					<p>НАПИШИТЕ КОММЕНТАРИЙ или хотя бы смайлик. Вам не сложно, а мне будет очень приятно!</p>
+				</td>        
+			</tr>
+			
+			
+			
+			<tr>
+				<td colspan="3" align="center"><h3>НОВИНКА!!!Супер НЕЖНЫЕ!!! Горячие бутерброды с КРАБОВЫМИ палочками. ОЧЕНЬ ВКУСНО!!!</h3></td>
+			</tr>
+			<tr>        
+				<td class="td-video">
+					<iframe width="560" height="315" src="https://www.youtube.com/embed/Kc1oJpXm3lQ?si=br98x_jcg9D0nJ2Y" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>					
+				</td>
+				<td class="td-description">			
+					<p>Вкусный, нежный творожно-ягодный десерт без выпечки! Очень полезный! Дети его просто обожают! </p>
+
+					<p>Ингредиенты:</p>
+
+					<p>творог – 200 г </p>
+					<p>молоко – 150 г</p>
+					<p>сахар – 40-50 г</p>
+					<p>желатин – 5-7 г</p>
+					<p>ягоды (фрукты) – 300 г</p>
+
+					<p>Приятного аппетита!</p>
+
+				</td>        
+			</tr>
+			
+			
+			
+			
+			<tr>
+				<td colspan="3" align="center"><h3>Очень вкусный и полезный ЧЕЧЕВИЧНЫЙ СУП по-итальянски с мясом и овощами. Полюбит вся семья!</h3></td>
+			</tr>
+			<tr>        
+				<td class="td-video">
+				<iframe width="560" height="315" src="https://www.youtube.com/embed/qiUroB_Uj1M?si=81YBmWJF-yOW5pvl" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+				</td>
+				<td class="td-description">			
+					<p> Чечевица частая гостья на нашем столе, ведь бобы чечевицы очень питательны, так как содержат большое количество белка, не уступающего по своей ценности мясу, но усваивается он гораздо легче.</p> 
+					<p>У чечевицы есть особенное свойство: она не накапливает токсины, поэтому, где бы ни выросла чечевица, она будет экологически чистой.</p>
+					<p>Ингредиенты:<p>
+					<p>	Чечевица - 250 г</p> 
+					<p>Вода - 2,5 л </p>
+					<p>	Мясо говядина - 600 г (можно любое)</p> 
+					<p>	Лук репчатый - 230 г </p>
+					<p>	Морковь - 110 г </p>
+					<p>	Картофель - 300 г </p>
+					<p>	Помидоры свежие - 200 г</p> 
+					<p>	Соль - 1 ст.л. </p>
+					<p>	Сладкая паприка молотая - 2 ч.л.</p> 
+					<p>	Перец черный молотый - 1 ч.л.</p> 
+					<p>	Чеснок - 3 зубчика </p>
+					<p>	Растительное масло - 2 ст.л.</p> 
+					<p>	Лавровый лист - 2 шт </p>
+					<p>	Томатная паста - 1 ст.л. </p>
+					<p>	Зелень петрушки или кинзы - 5 г</p>
+
+				</td>        
+			</tr>
+			
+			
+			
+		</table>
+	</body>
+</html>
+
+
+
+
+
+
+
